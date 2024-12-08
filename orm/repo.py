88d1579995bfaa2d9ca8ce679c54a7ba535ba592
AlexMtzGzn <alex13141926@gmail.com>
2024-12-08 10:55:54 -0600

    Regresa el alumno por id

--- orm/repo.py
+++ orm/repo.py
@@ -5,3 +5,20 @@ from sqlalchemy import and_
 def regresa_Alumnos(sesion:Session):
     print("SELECT * FROM app.alumnos")
     return sesion.query(modelos.Alumno).all()
+
+def regresa_Alumno_ID(sesion):
+    print("SELECT * FROM app.alumnos WHERE id = ", id_alumno)
+    return sesion.query(modelos.Alumno).filter(modelos.Alumno.id == id_usuario).first()
+
+
+"""▪ SELECT * FROM app.alumnos
+▪ SELECT * FROM app.alumnos WHERE id={id_al}
+▪ SELECT * FROM app.fotos
+▪ SELECT * FROM app.fotos WHERE id={id_fo}
+▪ SELECT * FROM app.fotos WHERE id_alumnos={id_al}
+▪ SELECT * FROM app.calificaciones
+▪ SELECT * FROM app.calificaciones WHERE id={id_fo}
+▪ SELECT * FROM app.calificaciones WHERE id_alumnos={id_al}
+▪ DELETE FROM app.alumnos WHERE id_alumnos={id_al}
+▪ DELETE FROM app.calificaciones WHERE id_alumnos={id_al}
+▪ DELETE FROM app.fotos WHERE id_alumnos={id_al}"""
