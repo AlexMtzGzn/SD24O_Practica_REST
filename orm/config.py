60f58c14e38788607d5084508a7e41fae3f79d4e
AlexMtzGzn <alex13141926@gmail.com>
2025-01-07 12:29:55 -0600

    base de datos

--- orm/config.py
+++ orm/config.py
@@ -1,13 +1,17 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
+from orm import modelos
+import os
 
-URL_BASE_DATOS = "postgresql://usuario_ejemplo:Scanner1314@localhost:5432/bd_alumnos"
+# URL_BASE_DATOS = "postgresql://usuario_ejemplo:Scanner1314@localhost:5432/bd_alumnos"
 
-engine = create_engine(URL_BASE_DATOS,
-                       connect_args={
-                           "options": "-csearch_path=app"  #Para que la BD se conecte por medio del esquema
-                       })
+# engine = create_engine(URL_BASE_DATOS,
+#                        connect_args={
+#                            "options": "-csearch_path=app"  #Para que la BD se conecte por medio del esquema
+#                        })
+
+engine = create_engine(os.getenv("db_url","sqlite://base-ejemplo.db"))
 
 SessionClass = sessionmaker(engine) 
 
